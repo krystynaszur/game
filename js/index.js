f0e1847db08732a0b11046114677b512bb48739f
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-06-15 00:15:30 +0200

    Update index.js to improve the modals

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 var gameSettings = {
-playerWins: 0,
-computerWins: 0,
-maxRounds: 0,
-playedRounds: 0,
-endOfGame: undefined,
-progress: []
+  playerWins: 0,
+  computerWins: 0,
+  maxRounds: 0,
+  playedRounds: 0,
+  endOfGame: undefined,
+  progress: [],
+  enteredUsername: undefined
 };
 
-var random = function(items)
-{
-return items[Math.floor(Math.random()*items.length)];  
+var random = function(items) {
+  return items[Math.floor(Math.random()*items.length)];  
 }
+
 var items = [1,2,3];
 
 var computerMove = function() {
@@ -34,81 +35,117 @@ var playerMove = function(move, computer) {
     winner: ''
   };
   
- if (move == 'paper') {
-    if (computer == 'paper') {
-    result.text = 'It is a draw.';
-    result.winner = 'draw';
+  if (move == 'paper') {
+      if (computer == 'paper') {
+        result.text = 'It is a draw.';
+        result.winner = 'draw';
+      } 
+      else if (computer == 'scissors') {
+        result.text = 'You lose.';
+        result.winner = 'computer';
+      } 
+      else if (computer == 'rock') {
+        result.text = 'You win.';
+        result.winner = 'player';
+      }
     } 
-    else if (computer == 'scissors') {
-    result.text = 'You lose.';
-    result.winner = 'computer';
-    } 
-    else if (computer == 'rock') {
-    result.text = 'You win.';
-    result.winner = 'player';
-    }
-} else if (move == 'scissors') {
-    if (computer == 'paper') {
-    result.text = 'You win.';
-    result.winner = 'player';
-    } 
-    else if (computer == 'scissors') {
-    result.text = 'It is a draw.';
-    result.winner = 'draw';
-    } 
-    else if (computer == 'rock') {
-    result.text = 'You lose.';
-    result.winner = 'computer';
-    }
-} else if (move == 'rock') {
-    if (computer == 'paper') {
-    result.text = 'You lose.';
-    result.winner = 'computer';
-    } 
-    else if (computer == 'scissors') {
-    result.text = 'You win. ';
-    result.winner = 'player';
-    } 
-    else if (computer == 'rock') {
-    result.text = 'It is a draw. ';
-    result.winner = 'draw';
-    }
-}
-return result;
+  else if (move == 'scissors') {
+      if (computer == 'paper') {
+        result.text = 'You win.';
+        result.winner = 'player';
+      } 
+      else if (computer == 'scissors') {
+        result.text = 'It is a draw.';
+        result.winner = 'draw';
+      } 
+      else if (computer == 'rock') {
+        result.text = 'You lose.';
+        result.winner = 'computer';
+      }
+  } 
+  else if (move == 'rock') {
+      if (computer == 'paper') {
+        result.text = 'You lose.';
+        result.winner = 'computer';
+      } 
+      else if (computer == 'scissors') {
+        result.text = 'You win. ';
+        result.winner = 'player';
+      } 
+      else if (computer == 'rock') {
+        result.text = 'It is a draw. ';
+        result.winner = 'draw';
+      }
+  }
+  return result;
 };
 
-var showResult = function(roundResult, move, computer) {
+var showRoundResult = function(roundResult, move, computer) {
   var output = document.getElementById("output");
   output.innerHTML = roundResult.text + ' You chose ' + move + ' while the computer chose '+ computer + '.';
 }
 
-var gameResult = function(roundResult) {
-var result = document.getElementById('result');
+var showRoundPoints = function(roundResult) {
+  var result = document.getElementById('round-result');
   
-var playerWin=0;
+  var playerWin=0;
   var computerWin=0;
   
- if (roundResult.winner == "player") { //tworzysz tak warunek, by nie stosować tekstów z inner.HTML, tylko dane, które się nie zmieniają
-   playerWin++;
-    }
- else if (roundResult.winner == "computer") {
-  computerWin++;
-   }
-    
-result.innerHTML = "Round result: " + playerWin + "-" + computerWin;
+  if (roundResult.winner == "player") { //tworzysz tak warunek, by nie stosować tekstów z inner.HTML, tylko dane, które się nie zmieniają
+    playerWin++;
+  }
+  else if (roundResult.winner == "computer") {
+    computerWin++;
+  }    
+  result.innerHTML = "Round result: " + playerWin + "-" + computerWin;
 }
 
-var deactivateGame = function () {
-  if (gameSettings.playerWins == gameSettings.maxRounds ||gameSettings.computerWins == gameSettings.maxRounds) {  
-    gameEnd(event);
-  var welcome = document.getElementById('welcome');
-  welcome.innerHTML="Game over, please press the NEW GAME button!<br><br>";
+var showTotalResult = function(roundResult) {
   
-  document.getElementById("paper").setAttribute("disabled", "disabled");
-  document.getElementById("scissors").setAttribute("disabled", "disabled");
-  document.getElementById("rock").setAttribute("disabled", "disabled");
+  if (roundResult.winner == 'player') {
+    gameSettings.playerWins++;
   }
-}
+  else if (roundResult.winner == 'computer') {
+    gameSettings.computerWins++;
+  }
+
+  gameSettings.playedRounds++;
+  
+  if (gameSettings.playerWins == gameSettings.maxRounds) {
+    gameSettings.endOfGame = gameSettings.enteredUsername + ' won the entire game'
+  }
+  else if (gameSettings.computerWins == gameSettings.maxRounds) {
+    gameSettings.endOfGame = 'The computer won the entire game'
+  }
+  else {
+    gameSettings.endOfGame = 'Keep playing!'
+  }
+  
+  var total = document.getElementById('total-result');
+  total.innerHTML = "Total result is: " + 
+    gameSettings.playerWins + 
+    "-" + 
+    gameSettings.computerWins + 
+    '.<br>The number of played rounds is: ' + 
+    gameSettings.playedRounds+
+    '.<br>The number of points needed for victory is: ' + 
+    gameSettings.maxRounds+
+    '.<br>' +
+    gameSettings.endOfGame;
+ }
+
+var paperButton = document.getElementById('paper');
+var scissorsButton = document.getElementById('scissors');
+var rockButton = document.getElementById('rock');
+
+var buttons = document.querySelectorAll('.player-move');
+	
+for(var i = 0; i < buttons.length; i++) {
+  var chosenButton = buttons[i].getAttribute('data-move');
+  buttons[i].addEventListener('click', function() {
+    startRound(chosenButton);
+  });
+};
 
 var startRound = function(move) {
 
@@ -116,13 +153,13 @@ var startRound = function(move) {
     
     var computer = computerMove();
     var roundResult = playerMove(move, computer);
-    showResult(roundResult, move, computer);
-    gameResult (roundResult);
+    showRoundResult(roundResult, move, computer);
+    showRoundPoints(roundResult);
     showTotalResult(roundResult);
     deactivateGame();
   }
 
- var roundResultsForTable = { //tworzę obiekt
+  var roundResultsForTable = { //tworzysz obiekt do tabeli
     roundNumber: gameSettings.playedRounds,
     playerMove: move,
     computerMove: computer,
@@ -130,8 +167,7 @@ var startRound = function(move) {
     currentResult:  gameSettings.playerWins + "-" + gameSettings.computerWins 
   }
 
-  gameSettings.progress.push(roundResultsForTable); //przesuwam obiekt do tabeli
-  console.log(gameSettings.progress);
+  gameSettings.progress.push(roundResultsForTable); //przesuwasz obiekt do tabeli
 
   for(var i = 0; i < gameSettings.progress.length; i++) {
     var gameResultsTableRows ='';
@@ -146,126 +182,83 @@ var startRound = function(move) {
       '</td><td>' +
       gameSettings.progress[i].currentResult +
       '</td></tr>';
-    console.log(gameResultsTableRows);
   }
 
   var gameResultsRows = document.getElementById('game-results-table');
   gameResultsRows.innerHTML += gameResultsTableRows
 }  
 
-var paperButton = document.getElementById('paper');
-var scissorsButton = document.getElementById('scissors');
-var rockButton = document.getElementById('rock');
+var deactivateGame = function () {
+  if (gameSettings.playerWins == gameSettings.maxRounds ||gameSettings.computerWins == gameSettings.maxRounds) {  
+    gameEnd(event);
+    document.getElementById("paper").setAttribute("disabled", "disabled");
+    document.getElementById("scissors").setAttribute("disabled", "disabled");
+    document.getElementById("rock").setAttribute("disabled", "disabled");
+  }
+}
 
-var buttons = document.querySelectorAll('.player-move');
-	
-for(var i = 0; i < buttons.length; i++) {
-    var chosenButton = buttons[i].getAttribute('data-move');
-    buttons[i].addEventListener('click', function() {
-      startRound(chosenButton);
-    });
-};
-  
-var showTotalResult = function(roundResult) {
-  
-  if (roundResult.winner == 'player') {
-    gameSettings.playerWins++;
-  }
-  else if (roundResult.winner == 'computer') {
-    gameSettings.computerWins++;
-  }
-    gameSettings.playedRounds++;
-  
-  if (gameSettings.playerWins == gameSettings.maxRounds) {
-    gameSettings.endOfGame = 'You won the entire game'}
-  else if (gameSettings.computerWins == gameSettings.maxRounds) {
-    gameSettings.endOfGame = 'The computer won the entire game'}
-  else {
-    gameSettings.endOfGame = 'Keep playing!'}
-  
-  var total = document.getElementById('total result');
-  total.innerHTML = "Total result is: " + gameSettings.playerWins + "-" + gameSettings.computerWins + '.<br>The number of played rounds is: ' + gameSettings.playedRounds+'.'
-  + '<br>The number of points needed for victory is: ' + gameSettings.maxRounds+'.'+ '<br>' + gameSettings.endOfGame;
-  }
-
-  var activateButtons = function(enteredUsername, enteredWins) {
-    
-   gameSettings.maxRounds = enteredWins;
-
-    if (gameSettings.maxRounds >0) {
-      document.getElementById("paper").removeAttribute("disabled");
-      document.getElementById("scissors").removeAttribute("disabled");
-      document.getElementById("rock").removeAttribute("disabled");
-    }
-  }
-
-   var activateGame = function() {
-  
-    var enteredUsername = document.getElementById('username').value;
-    var enteredWins = document.getElementById('wins').value;
-    activateButtons(enteredUsername, enteredWins);
-  
-   /* if (gameSettings.maxRounds >0) {
-      document.getElementById("paper").removeAttribute("disabled");
-      document.getElementById("scissors").removeAttribute("disabled");
-      document.getElementById("rock").removeAttribute("disabled");
-    }*/
-  
-    document.querySelector('#modal-overlay').classList.remove('show');
-  }
- 
-  var enterGameSettings = function(event){
-    event.preventDefault();
-    document.getElementById('modal-overlay').classList.add('show');
-    document.getElementById('enter-settings').classList.add('show');
-    document.getElementById('game-results').classList.remove('show');
-    var request = document.getElementById('request');
-    request.innerHTML="Please decide about the settings!";
-  
+var enterGameSettings = function(event){
+  showModal(enterGameSettings);
+   
+  gameSettings.enteredUsername ="";
   gameSettings.maxRounds=0;
   gameSettings.playerWins =0;
   gameSettings.computerWins=0;
   gameSettings.playedRounds=0;
   
- /*var output = document.getElementById('output');
- output.innerHTML=" ";
-  var result = document.getElementById('result');
- result.innerHTML=" ";
-   var total = document.getElementById('total result');
- total.innerHTML=" "; //usuwasz wszystkie teksty, możesz to też zrobić za pomoc querySelectorAll, ale powstaje z tego tablica
- var gameResultsRows = document.getElementById('game-results-table');
- gameResultsRows.innerHTML="<tr><th>Round number: </th><th>Player move: </th><th>Computer move: </th><th>Round winner: </th><th>Current result: </th></tr>";
-  var welcome = document.getElementById('welcome');
-welcome.innerHTML="Good luck!<br><br>";*/
+  var enterUsername = document.getElementById('username')
+  enterUsername.value = "";
+  var enterWins = document.getElementById('wins')
+  enterWins.value = "";
 
 };
+
+var activateGame = function() {
+
+  gameSettings.enteredUsername = document.getElementById('username').value;
+  gameSettings.maxRounds = document.getElementById('wins').value;
+
+  if (gameSettings.maxRounds >0) {
+      document.getElementById("paper").removeAttribute("disabled");
+      document.getElementById("scissors").removeAttribute("disabled");
+      document.getElementById("rock").removeAttribute("disabled");
+      document.querySelector('#modal-overlay').classList.remove('show');
+  }
+}
+ 
 var submit = document.getElementById("submit-button");
 submit.addEventListener('click', activateGame);
 
 var newGame = document.getElementById('new-game');
-
 newGame.addEventListener('click', enterGameSettings);
 
 var gameEnd = function(event){
-  event.preventDefault();
-  document.getElementById('modal-overlay').classList.add('show');
-  document.getElementById('game-end').classList.add('show');
-  document.getElementById('enter-settings').classList.remove('show');
-
+  showModal(gameEnd);
+  
   var showGameResults = document.getElementById('game-results');
-  showGameResults.innerHTML ="Game over, please press the NEW GAME button!<br><br> Total result is: " + gameSettings.playerWins + "-" + gameSettings.computerWins + "<br><br> " + gameSettings.endOfGame + "<br><br>Here is the game progress: <br><br> ";
+  showGameResults.innerHTML ="Game over, please press the NEW GAME button!<br><br> Total result is: " + 
+  gameSettings.playerWins + 
+  "-" + 
+  gameSettings.computerWins + 
+  "<br><br> " + 
+  gameSettings.endOfGame + 
+  "<br><br>Here is the game progress: <br><br> ";
 
   var output = document.getElementById('output');
   output.innerHTML=" ";
-  var result = document.getElementById('result');
+  var result = document.getElementById('round-result');
   result.innerHTML=" ";
-    var total = document.getElementById('total result');
+  var total = document.getElementById('total-result');
   total.innerHTML=" ";
 };
 
 var hideModal = function(event){
   event.preventDefault();
-  document.querySelector('#modal-overlay').classList.remove('show');
+
+  var visibleModals = document.querySelectorAll(".show");
+  for(var i = 0; i < visibleModals.length; i++){
+    visibleModals[i].classList.remove('show');
+  };
 };
 
 var closeButtons = document.querySelectorAll('.modal .close');
@@ -284,4 +277,17 @@ for(var i = 0; i < modals.length; i++){
   });
 }
 
+var showModal = function(typeOfModal){
+  event.preventDefault();
 
+  document.getElementById('modal-overlay').classList.add('show');
+
+  if (typeOfModal == enterGameSettings) {
+    document.getElementById('enter-settings').classList.add('show');
+    document.getElementById('game-end').classList.remove('show');
+  }
+  else if (typeOfModal == gameEnd) {
+    document.getElementById('enter-settings').classList.remove('show');
+    document.getElementById('game-end').classList.add('show');
+  }
+}
